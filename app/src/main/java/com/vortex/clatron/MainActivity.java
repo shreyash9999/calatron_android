d69cefe5566d2111d281d242df374202c3ff2bb1
shreyash9999 <dongareshreyash20@gmail.com>
2023-07-19 13:38:57 +0530

    calatorn

---
+++ app/src/main/java/com/vortex/clatron/MainActivity.java
@@ -0,0 +1,25 @@
+package com.vortex.clatron;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+
+
+
+
+
+
+    }
+}
